141ba56a54c0e2fe001ec178e648243fb0cdbec1
Denis_Tihonov <ndenmru@gmail.com>
2025-05-17 10:07:19 +0300

    Add folder project LightAndDark

---
+++ LightAndDark/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace LightAndDark
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ltheme = new System.Windows.Forms.Label();
+            this.SwithDarkWhite = new Siticone.UI.WinForms.SiticoneWinToggleSwith();
+            this.SuspendLayout();
+            // 
+            // ltheme
+            // 
+            this.ltheme.AutoSize = true;
+            this.ltheme.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ltheme.Location = new System.Drawing.Point(676, 9);
+            this.ltheme.Name = "ltheme";
+            this.ltheme.Size = new System.Drawing.Size(66, 18);
+            this.ltheme.TabIndex = 0;
+            this.ltheme.Text = "THEME";
+            // 
+            // SwithDarkWhite
+            // 
+            this.SwithDarkWhite.Location = new System.Drawing.Point(744, 5);
+            this.SwithDarkWhite.Name = "SwithDarkWhite";
+            this.SwithDarkWhite.Size = new System.Drawing.Size(44, 25);
+            this.SwithDarkWhite.TabIndex = 1;
+            this.SwithDarkWhite.Text = "SwithDarkWhite";
+            this.SwithDarkWhite.CheckedChanged += new System.EventHandler(this.SwithDarkWhite_CheckedChanged);
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.SwithDarkWhite);
+            this.Controls.Add(this.ltheme);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form";
+            this.Text = "LightAndDark";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label ltheme;
+        private Siticone.UI.WinForms.SiticoneWinToggleSwith SwithDarkWhite;
+    }
+}
+
---
+++ LightAndDark/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LightAndDark
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form()
+        {
+            InitializeComponent();
+            getInit();
+        }
+
+        private void getInit()
+        {
+            Settings get = new Settings();
+            get.readIni();
+
+            if (get.theme == "dark")
+            {
+                SwithDarkWhite.Checked = true;
+                ltheme.Text = "DARK";
+                this.BackColor = Color.FromArgb(32, 33, 36);
+                this.ForeColor = Color.White;
+            }
+            else
+            {
+                SwithDarkWhite.Checked = false;
+                ltheme.Text = "LIGHT";
+                this.BackColor = Color.White;
+                this.ForeColor = Color.FromArgb(32, 33, 36);
+            }
+        }
+
+        private void SwithDarkWhite_CheckedChanged(object sender, EventArgs e)
+        {
+            Settings set = new Settings();
+            if (SwithDarkWhite.Checked == true)
+            {
+                set.writeIni("SECTION", "key", "dark");
+                this.BackColor = Color.FromArgb(32, 33, 36);
+                this.ForeColor = Color.White;
+                ltheme.Text = "DARK";
+            }
+            else
+            {
+                set.writeIni("SECTION", "key", "light");
+                this.BackColor = Color.White;
+                this.ForeColor = Color.FromArgb(32, 33, 36);
+                ltheme.Text = "LIGHT";
+            }
+        }
+    }
+}
